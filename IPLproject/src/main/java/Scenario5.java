948ae09442e5a1fd96416d54e7b70f0b1a31a1e1
Aniket Harayya <aniket.harayya.20@mountblue.tech>
2022-05-22 22:58:54 +0530

    Changes Updated

--- IPLproject/src/main/java/Scenario5.java
+++ IPLproject/src/main/java/Scenario5.java
@@ -3,21 +3,17 @@ import java.util.List;
 import java.util.Map;
 
 public class Scenario5 {
-public static Map<String,Integer> WinbatWinBall(List<MatchData> matches)
-{
-    Map<String,Integer> WinBB = new HashMap<>();
-    for(MatchData m:matches)
-    {
-        if(WinBB.containsKey(m.getToss_winner()))
-        {
-           int count = WinBB.get(m.getToss_winner()) ;
-            WinBB.replace(m.getToss_winner(),count+1);
+public static Map<String,Integer> WinbatWinBall(List<MatchData> matches) {
+    Map<String,Integer> winByBatBall = new HashMap<>();
+    for(MatchData m:matches) {
+        if(winByBatBall.containsKey(m.getToss_winner())) {
+           int count = winByBatBall.get(m.getToss_winner()) ;
+            winByBatBall.replace(m.getToss_winner(),count+1);
         }
-        else
-        {
-            WinBB.put(m.getToss_winner(),1);
+        else {
+            winByBatBall.put(m.getToss_winner(),1);
         }
     }
-    return WinBB;
+    return winByBatBall;
 }
 }
